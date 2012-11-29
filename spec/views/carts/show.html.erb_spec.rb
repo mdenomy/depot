219b29dbe5a4147fc06d71bb11cd71bdf3f9df95
Michael Denomy <mdenomy@gmail.com>
2012-11-28 22:25:27 -0500

    Added quanity to line_item and using partial view for line_item

--- spec/views/carts/show.html.erb_spec.rb
+++ spec/views/carts/show.html.erb_spec.rb
@@ -2,15 +2,22 @@ require 'spec_helper'
 
 describe "carts/show" do
   before(:each) do
-    line_item1 = create(:line_item, product: create(:product, title: "Hey Hey"))
-    line_item2 = create(:line_item, product: create(:product, title: "My My"))
-    @cart = create(:cart, line_items: [line_item1, line_item2])
+    @line_item1 = create(:line_item, product: create(:product,
+                                                     title: "Hey Hey"))
+    @line_item2 = create(:line_item, product: create(:product,
+                                                     title: "My My"))
+    @cart = create(:cart, line_items: [@line_item1, @line_item2])
   end
 
   it "shows line items in cart" do
     render
-    assert_select 'div.line_item', 2
-    assert_select 'div.line_item', {text: "Hey Hey"}
-    assert_select 'div.line_item', {text: "My My"}
+    assert_select 'tr', 2
+  end
+
+  it "shows quantity and title" do
+    render :partial => "line_items/line_item",
+           :locals => {line_item: @line_item2}
+    assert_select 'td', {text:"1&times;"}
+    assert_select 'td', {text:"My My"}
   end
 end
