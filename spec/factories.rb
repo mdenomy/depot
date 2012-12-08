c75ab8b9ed7fca60a340e43acbf986093c5d4648
Michael Denomy <mdenomy@gmail.com>
2012-12-08 15:37:50 -0500

    Fixed factory to make unique titles

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :product do
-    title       Faker::Lorem.sentence(4)
+    sequence(:title) {|n| "Book#{n}" }
     description Faker::Lorem.sentence(20)
     image_url   "test.png"
     price       Random.rand(10...75)
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -24,8 +24,8 @@ describe Cart do
     end
 
     it "should calculate the total" do
-      @line_item1 = create(:line_item, product: create(:product, title: "A", price: 33.33), quantity: 1)
-      @line_item2 = create(:line_item, product: create(:product, title: "B", price: 50.00), quantity: 2)
+      @line_item1 = create(:line_item, product: create(:product, price: 33.33), quantity: 1)
+      @line_item2 = create(:line_item, product: create(:product, price: 50.00), quantity: 2)
       @cart = create(:cart, line_items: [@line_item1, @line_item2])
       @cart.total.should eq 133.33
     end
